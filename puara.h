3894e66e3b1852c5cb41909bbe95c8f41bce5004
Paul Buser <buser.paul@gmail.com>
2022-10-19 13:36:20 -0400

    cleanup

--- puara.h
+++ puara.h
@@ -50,7 +50,7 @@ class Puara {
     private:
         static unsigned int version;
         static std::string dmiName;
-        
+
         struct settingsVariables {
             std::string name;
             std::string type;
